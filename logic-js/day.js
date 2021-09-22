a640c53e4276bd988c84c058ba2f0b15f24b84c1
Pinkshoelace <75367343+Pinkshoelace@users.noreply.github.com>
2021-09-22 09:04:50 +0200

    Create day.js

---
+++ logic-js/day.js
@@ -0,0 +1,25 @@
+const getDay = () => {
+    switch (//new Date().getDay()
+            1
+    ) {
+        case 0:
+          return "sunday";
+        case 1:
+            return "monday";
+        case 2:
+            return "tuesday";
+        case 3:
+            return "wednesday";
+        case 4:
+            return "thursday";
+        case 5:
+            return "friday";
+        case 6:
+            return "saturday";
+      }
+}
+
+let day =  getDay()
+export {
+    day
+}
